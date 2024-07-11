abb7aa60dbfaa0ab81351ca85234212429f28149
AdminCheck <denis.kabirov.95@mail.ru>
2024-07-11 21:32:36 +0500

    p1

---
+++ pprint.py
@@ -0,0 +1,14 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        pass
+
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def speak(self):
+        return "Woof!"
